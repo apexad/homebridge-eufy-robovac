05dbcd2cc3a8e86d6cee915b9cac2c1e22296cd5
Josh Strange <josh@joshstrange.com>
2019-05-17 14:09:38 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # homebridge-eufy-robovac
 Homebridge plugin for Eufy RoboVac
+
+### Features
+
+* Switch on / off. When off, it will returning to charging dock automatically.
+
+* Display battery level, and notify on low battery.
+
+* Display battery charging state.
+
+* Find robot
+
+### Installation
+
+1. Install required packages.
+
+   ```
+   npm install -g homebridge-eufy-robovac
+   ```
+   
+2. Add these values to `config.json`.
+
+    ```
+      "accessories": [
+        {
+          "accessory": "Eufy RoboVac",
+          "name": "Vacuum Cleaner",
+          "ip": "<optional ip address, this or deviceId must be set>",
+          "deviceId": "<optional deviceId, this or ip must be set>",
+          "localKey": "<localKey>"
+        }
+      ]
+    ``` 
+    You can find out more about the `deviceId`/`localKey` [here](https://github.com/joshstrange/eufy-rob)
+
+3. Restart Homebridge, and your Eufy RoboVac will be added to Home app.
+
+
+
+### Thank You
+
+* [mitchellrj](https://github.com/mitchellrj) - Did most of the legwork in figuring out how to talk to the Eufy
+* [seikan](https://github.com/seikan) - Provided a [great example](https://github.com/seikan/homebridge-xiaomi-mi-robot-vacuum) for how to expose a vacuum cleaner in homebridge/homekit
+
+
+## Development
+
+This plugin is written in TypeScript. You should just need to run `npm run build` after making changes in the `src/` directory.
+
+Also this plugin is dependant on [eufy-robovac](https://github.com/joshstrange/eufy-robovac/) so you will probably want to fork that repo as well.
---
+++ src/index.ts
@@ -0,0 +1,178 @@
+import {RoboVac, StatusResponse, WorkStatus} from 'eufy-robovac';
+
+let Accessory: any, Service: any, Characteristic: any, UUIDGen: any;
+
+
+module.exports = function(homebridge: any) {
+	Accessory = homebridge.platformAccessory;
+	Service = homebridge.hap.Service;
+	Characteristic = homebridge.hap.Characteristic;
+
+	homebridge.registerAccessory('homebridge-eufy-robovac', 'Eufy RoboVac', EufyRoboVacAccessory);
+};
+
+class EufyRoboVacAccessory {
+	log: any;
+	config: { name?: string, deviceId?: string, localKey: string, ip?: string };
+	services: any[];
+	name: string;
+
+	fanService: any;
+	batteryService: any;
+	serviceInfo: any;
+	findRobot: any;
+
+	roboVac: RoboVac;
+
+	constructor(log: any, config: any) {
+		this.log = log;
+		this.config = config;
+		this.services = [];
+		this.name = this.config.name || 'Eufy RoboVac';
+
+		// Vacuum cleaner is not available in Homekit yet, register as Fan
+
+		this.serviceInfo = new Service.AccessoryInformation();
+
+		this.serviceInfo
+			.setCharacteristic(Characteristic.Manufacturer, 'Eufy')
+			.setCharacteristic(Characteristic.Model, 'RoboVac');
+
+		this.services.push(this.serviceInfo);
+
+		this.fanService = new Service.Fan(this.name);
+
+		this.fanService
+			.getCharacteristic(Characteristic.On)
+			.on('get', this.getCleanState.bind(this))
+			.on('set', this.setCleanState.bind(this));
+
+		this.services.push(this.fanService);
+
+		this.batteryService = new Service.BatteryService(this.name + ' Battery');
+		this.batteryService
+			.getCharacteristic(Characteristic.BatteryLevel)
+			.on('get', this.getBatteryLevel.bind(this));
+
+		this.batteryService
+			.getCharacteristic(Characteristic.ChargingState)
+			.on('get', this.getChargingState.bind(this));
+
+		this.batteryService
+			.getCharacteristic(Characteristic.StatusLowBattery)
+			.on('get', this.getStatusLowBattery.bind(this));
+
+		this.services.push(this.batteryService);
+
+		this.findRobot = new Service.Switch("Find " + this.name);
+
+		this.findRobot
+			.getCharacteristic(Characteristic.On)
+			.on('get', this.getFindRobot.bind(this))
+			.on('set', this.setFindRobot.bind(this));
+
+		this.services.push(this.findRobot);
+
+		this.setup();
+	}
+
+	async setup() {
+		this.roboVac = new RoboVac(this.config);
+		let status = await this.roboVac.getStatuses();
+		this.updateCleaningState(status.dps[this.roboVac.PLAY_PAUSE] as boolean);
+		this.updateBatteryLevel(status.dps[this.roboVac.BATTERY_LEVEL]);
+		this.updateChargingState(status.dps[this.roboVac.WORK_STATUS] === WorkStatus.CHARGING ? Characteristic.ChargingState.CHARGING : Characteristic.ChargingState.NOT_CHARGEABLE);
+		this.updateFindRobot(status.dps[this.roboVac.FIND_ROBOT] as boolean);
+
+		this.roboVac.api.on('data', (data: StatusResponse) => {
+			if(this.roboVac.PLAY_PAUSE in (data.dps as any)) {
+				this.updateCleaningState((data.dps as any)[this.roboVac.PLAY_PAUSE]);
+			}
+
+			if(this.roboVac.BATTERY_LEVEL in (data.dps as any)) {
+				this.updateBatteryLevel((data.dps as any)[this.roboVac.BATTERY_LEVEL]);
+			}
+
+			if(this.roboVac.WORK_STATUS in (data.dps as any)) {
+				this.updateBatteryLevel((data.dps as any)[this.roboVac.WORK_STATUS] === WorkStatus.CHARGING ? Characteristic.ChargingState.CHARGING : Characteristic.ChargingState.NOT_CHARGEABLE);
+			}
+
+			if(this.roboVac.FIND_ROBOT in (data.dps as any)) {
+				this.updateFindRobot((data.dps as any)[this.roboVac.FIND_ROBOT]);
+			}
+		});
+	}
+
+	updateCleaningState(state: boolean) {
+		this.log.debug('Cleaning State -> %s', state);
+		this.fanService.getCharacteristic(Characteristic.On).updateValue(state)
+	}
+
+	async getCleanState(callback: Function) {
+		this.log.debug("getCleanState");
+		callback(null, await this.roboVac.getPlayPause());
+	}
+
+	async setCleanState(state: boolean, callback: Function) {
+		this.log.debug("setCleanState", state);
+		await this.roboVac.setPlayPause(state);
+		if(!state) {
+			await sleep(2000);
+			await this.roboVac.goHome();
+		}
+		setTimeout(async () => {
+			this.updateCleaningState(await this.roboVac.getPlayPause());
+		}, 1000);
+
+		callback();
+	}
+
+	updateBatteryLevel(level: any) {
+		this.log.debug('Battery Level -> %s', level);
+		this.batteryService.getCharacteristic(Characteristic.BatteryLevel).updateValue(level);
+	}
+
+	async getBatteryLevel(callback: Function) {
+		this.log.debug("getBatteryLevel");
+		callback(null, await this.roboVac.getBatteyLevel());
+	}
+
+	updateChargingState(state: any) {
+		this.log.debug('Charging State -> %s', state);
+		this.batteryService.getCharacteristic(Characteristic.ChargingState).updateValue(state);
+	}
+
+	async getChargingState(callback: Function) {
+		callback(null, (await this.roboVac.getWorkStatus() === WorkStatus.CHARGING) ? Characteristic.ChargingState.CHARGING : Characteristic.ChargingState.NOT_CHARGEABLE);
+	}
+
+	async getStatusLowBattery(callback: Function) {
+		callback(null, (await this.roboVac.getBatteyLevel() < 30) ? Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW : Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL);
+	}
+
+	updateFindRobot(state: boolean) {
+		this.log.debug('Find Robot -> %s', state);
+		this.findRobot.getCharacteristic(Characteristic.On).updateValue(state);
+	}
+
+	async getFindRobot(callback: Function) {
+		callback(null, await this.roboVac.getFindRobot());
+	}
+
+	async setFindRobot(state: boolean, callback: Function) {
+		await this.roboVac.setFindRobot(state);
+		callback();
+	}
+
+	identify(callback: Function) {
+		callback();
+	}
+	getServices() {
+		return this.services;
+	}
+}
+
+
+function sleep(ms: number) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
