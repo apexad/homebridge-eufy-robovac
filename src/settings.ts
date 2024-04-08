b8f05d30cb28f57efda5b6c940df9230937d2554
Christian Hovestadt <accounts@hovestadt.me>
2024-04-08 09:58:19 +0200

    fixed platform and plugin name

--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,9 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'HomebridgeEufyRobovac';
+export const PLATFORM_NAME = 'EufyRobovac';
 
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-eufy-robovac';
+export const PLUGIN_NAME = '@hov3rcraft/homebridge-eufy-robovac';
